13596edbc3436ac257341206e51b13134cc2a2af
SolarisAdams <solarisadams@gmail.com>
2019-07-05 14:06:52 +0800

    Revert "Revert "buttons""
    This reverts commit 757d285541663a5554dfdbeec698416db0507f6f.

--- bank_ms/static/js/audio.js
+++ bank_ms/static/js/audio.js
@@ -1,391 +1,395 @@
-;(function($){
-	
-	var fnName = 'audioPlay';
-	var config = {
+;(function ($) {
 
-		view : ".audio-view",
+    var fnName = 'audioPlay';
+    var config = {
 
-		title : ".audio-title",
+        view: ".audio-view",
 
-		cover : ".audio-cover",
+        title: ".audio-title",
 
-		autoPlay : false,
+        cover: ".audio-cover",
 
-		volume : {
+        autoPlay: false,
 
-			volumeView : ".audio-set-volume",
-			volumeBox : ".volume-box",
-		},
+        volume: {
 
-		timeView : {
+            volumeView: ".audio-set-volume",
+            volumeBox: ".volume-box",
+        },
 
-			thisTime : ".audio-this-time",
+        timeView: {
 
-			countTime : '.audio-count-time',
-		},
+            thisTime: ".audio-this-time",
 
-		setbacks : {
+            countTime: '.audio-count-time',
+        },
 
-			setbacks : '.audio-setbacks',
+        setbacks: {
 
-			thisSetbacks : '.audio-this-setbacks',
+            setbacks: '.audio-setbacks',
 
-			cacheSetbacks : ".audio-cache-setbacks",
+            thisSetbacks: '.audio-this-setbacks',
 
-			volumeSetbacks : ".volume-box > i",
+            cacheSetbacks: ".audio-cache-setbacks",
 
-			volumeCircular : ".volume-box > i span"
-		},
-			
-		button : {
+            volumeSetbacks: ".volume-box > i",
 
-			volume : ".audio-volume",
+            volumeCircular: ".volume-box > i span"
+        },
 
-			backs : ".audio-backs-btn",
+        button: {
 
-			prev : ".audio-prev",
+            volume: ".audio-volume",
 
-			play : ".audio-play",
+            backs: ".audio-backs-btn",
 
-			next : ".audio-next",
+            prev: ".audio-prev",
 
-			menu : ".audio-menu",
+            play: ".audio-play",
 
-			menuClose : ".menu-close"
-		},
+            next: ".audio-next",
 
-		menu : {
+            menu: ".audio-menu",
 
-			menuView : '.audio-list',
+            menuClose: ".menu-close"
+        },
 
-			colse : '.close',
+        menu: {
 
-			list : '.audio-inline'
-		},
+            menuView: '.audio-list',
 
-		song : null
-	};
+            colse: '.close',
 
-	var songEq = 0,
-		volumeSize = 0.7;
+            list: '.audio-inline'
+        },
 
-	window[fnName] = function(setConfig){
+        song: null
+    };
 
-		//设置属性值
-		if(typeof(setConfig) == "object"){
+    var songEq = 0,
+        volumeSize = 0.7;
 
-			for( var n in setConfig){
+    window[fnName] = function (setConfig) {
 
-				config[n] = setConfig[n];
-			}
-		}
+        //设置属性值
+        if (typeof (setConfig) == "object") {
 
-		var _this = config,
-			playDate;
+            for (var n in setConfig) {
 
-		var cover = $(_this.cover),
-			title = $(_this.title),
-			thisTime = $(_this.timeView.thisTime),
-			countTime = $(_this.timeView.countTime),
-			thisSetbacks = $(_this.setbacks.thisSetbacks),
-			cacheSetbacks = $(_this.setbacks.cacheSetbacks),
-			setbacks = $(_this.setbacks.setbacks),
-			volumeCircular = $(_this.setbacks.volumeCircular),
-			volumeSetbacks = $(_this.setbacks.volumeSetbacks),
-			volumeBox = $(_this.volume.volumeBox),
-			play = $(_this.button.play),
-			prev = $(_this.button.prev),
-			next = $(_this.button.next),
-			menuBtn = $(_this.button.menu),
-			volume = $(_this.button.volume),
-			menuClose = $(_this.button.menuClose),
-			backs = $(_this.button.backs);
+                config[n] = setConfig[n];
+            }
+        }
 
-		_this.createAudio = function(){
+        var _this = config,
+            playDate;
 
-			if(!_this.audio){
+        var cover = $(_this.cover),
+            title = $(_this.title),
+            thisTime = $(_this.timeView.thisTime),
+            countTime = $(_this.timeView.countTime),
+            thisSetbacks = $(_this.setbacks.thisSetbacks),
+            cacheSetbacks = $(_this.setbacks.cacheSetbacks),
+            setbacks = $(_this.setbacks.setbacks),
+            volumeCircular = $(_this.setbacks.volumeCircular),
+            volumeSetbacks = $(_this.setbacks.volumeSetbacks),
+            volumeBox = $(_this.volume.volumeBox),
+            play = $(_this.button.play),
+            prev = $(_this.button.prev),
+            next = $(_this.button.next),
+            menuBtn = $(_this.button.menu),
+            volume = $(_this.button.volume),
+            menuClose = $(_this.button.menuClose),
+            backs = $(_this.button.backs);
 
-				_this.audio = new Audio();
-			}
+        _this.createAudio = function () {
 
-			var song = config.song;
-			if(!song){
+            if (!_this.audio) {
 
-				alert('当前歌单没有歌曲!!!');
-				return false;
-			}
+                _this.audio = new Audio();
+            }
 
-			_this.stopAudio();
-			_this.audio.src = song[songEq].src;
-			
-			_this.volumeSet();
+            var song = config.song;
+            if (!song) {
 
-			title.text(song[songEq].title || '未知歌曲');
-			cover.css({
-				'backgroundImage' : 'url('+(song[songEq].cover || '')+')'
-			});
+                alert('当前歌单没有歌曲!!!');
+                return false;
+            }
 
-			function setDuration(){
+            _this.stopAudio();
+            _this.audio.src = song[songEq].src;
 
-				if(isNaN(_this.audio.duration)){
+            _this.volumeSet();
 
-					setTimeout(setDuration,50);
-				}else{
+            title.text(song[songEq].title || '未知歌曲');
+            cover.css({
+                'backgroundImage': 'url(' + (song[songEq].cover || '') + ')'
+            });
 
-					countTime.text(_this.conversion(_this.audio.duration));
-				}
-			}
-			setDuration(_this.audio.duration);
-			
-			thisTime.text(_this.conversion(_this.audio.currentTime));
+            function setDuration() {
 
-			_this.audio.onended = function(){
+                if (isNaN(_this.audio.duration)) {
 
-				setTimeout(function(){
+                    setTimeout(setDuration, 50);
+                } else {
 
-					++songEq;
-					songEq = (songEq < _this.song.length) ? songEq : 0;
-					_this.selectMenu(songEq,true);
-				},1000);
-			}
+                    countTime.text(_this.conversion(_this.audio.duration));
+                }
+            }
 
-		}
+            setDuration(_this.audio.duration);
 
-		var timeAudio;
-		_this.playAudio = function(){
+            thisTime.text(_this.conversion(_this.audio.currentTime));
 
-			if(_this.audio){
-				
-				if(!playDate || (Date.now() - playDate) > 100){
+            _this.audio.onended = function () {
 
-					playDate = Date.now();
+                setTimeout(function () {
 
-					(!_this.audio.paused) || _this.audio.pause();
+                    ++songEq;
+                    songEq = (songEq < _this.song.length) ? songEq : 0;
+                    _this.selectMenu(songEq, true);
+                }, 1000);
+            }
 
-					_this.audio.play();
-					play.addClass('audio-stop').one('click',function(){
+        }
 
-						_this.stopAudio();
-						$(this).removeClass('audio-stop').one('click',function(){
-							
-							_this.playAudio();
-						});
-					});
-					
-					timeAudio = setInterval(function(){
+        var timeAudio;
+        _this.playAudio = function () {
 
-						if(_this.audio.readyState == 4){
+            if (_this.audio) {
 
-							cacheSetbacks.css({
-								'width' : (_this.audio.buffered.end(0) / _this.audio.duration)*100+"%"
-							});
-						}
-						
-						thisSetbacks.css({
-							'width' : (_this.audio.currentTime / _this.audio.duration)*100+"%"
-						});
+                if (!playDate || (Date.now() - playDate) > 100) {
 
-						thisTime.text(_this.conversion(_this.audio.currentTime));
-					},500);
-				}else{
+                    playDate = Date.now();
 
-					setTimeout(function(){
+                    (!_this.audio.paused) || _this.audio.pause();
 
-						_this.playAudio();
-					},50);
-				}			
-			}
-		}
+                    _this.audio.play();
+                    play.addClass('audio-stop').one('click', function () {
 
-		_this.stopAudio = function(){
+                        _this.stopAudio();
+                        $(this).removeClass('audio-stop').one('click', function () {
 
-			if(!playDate || (Date.now() - playDate) > 100){
-	
-				playDate = Date.now();
-				_this.audio.pause();
-				clearInterval(timeAudio);
-			}else{
-				
-				setTimeout(function(){
+                            _this.playAudio();
+                        });
+                    });
 
-					_this.stopAudio();
-				},50);
-			}		
-		}
+                    timeAudio = setInterval(function () {
 
-		_this.conversion = function(num){
+                        if (_this.audio.readyState == 4) {
 
-			function changInt(num){
+                            cacheSetbacks.css({
+                                'width': (_this.audio.buffered.end(0) / _this.audio.duration) * 100 + "%"
+                            });
+                        }
 
-				return (num < 10) ? '0'+num : num;
-			}
+                        thisSetbacks.css({
+                            'width': (_this.audio.currentTime / _this.audio.duration) * 100 + "%"
+                        });
 
-			return changInt(parseInt(num/60))+":"+ changInt(Math.floor(num%60));
-		}
-		
-		_this.upMenu = function(){
+                        thisTime.text(_this.conversion(_this.audio.currentTime));
+                    }, 500);
+                } else {
 
-			var song = _this.song,
-				inline = $(_this.menu.list).empty();
+                    setTimeout(function () {
 
-			for(var i in song){
+                        _this.playAudio();
+                    }, 50);
+                }
+            }
+        }
 
-				inline.append("<li><a href='javascript:;'>"+(song[i].title || '52player.cn提示：未知歌曲')+"</a></li>");
-			}
+        _this.stopAudio = function () {
 
-			inline.find(">li").unbind('click').on('click',function(){
+            if (!playDate || (Date.now() - playDate) > 100) {
 
-				_this.selectMenu($(this).index(),true);
-			});
-		}
+                playDate = Date.now();
+                _this.audio.pause();
+                clearInterval(timeAudio);
+            } else {
 
-		_this.selectMenu = function(num,_bool){
+                setTimeout(function () {
 
-			songEq = num;
-			_this.createAudio();
-			(_bool) && _this.playAudio();
-		}
+                    _this.stopAudio();
+                }, 50);
+            }
+        }
 
-		_this.volumeSet = function(){
+        _this.conversion = function (num) {
 
-			_this.audio.volume = volumeSize;
-			volumeSetbacks.css({
-				'height' : volumeSize*100 + "%"
-			});
-		}
+            function changInt(num) {
 
-		_this.newSong = function(_new,_bool){
+                return (num < 10) ? '0' + num : num;
+            }
 
-			if( typeof(_new) == 'object' ){
+            return changInt(parseInt(num / 60)) + ":" + changInt(Math.floor(num % 60));
+        }
 
-				if(_new.src){
+        _this.upMenu = function () {
 
-					if(_this.song){
+            var song = _this.song,
+                inline = $(_this.menu.list).empty();
 
-						_this.song.push(_new);
-					}else{
+            for (var i in song) {
 
-						_this.song = [_new];
-					}
-					
-					_this.upMenu();
-					(_bool) && _this.selectMenu(_this.song.length-1,true);
-				}else{
+                inline.append("<li><a href='javascript:;'>" + (song[i].title || '提示：未知歌曲') + "</a></li>");
+            }
 
-					alert('52player.cn提示：对象缺省src属性');
-				}
-			}else{
+            inline.find(">li").unbind('click').on('click', function () {
 
-				alert('52player.cn提示：这不是一个对象');
-			}
-		}
+                _this.selectMenu($(this).index(), true);
+            });
+        }
 
-		var volumeTime;
-		volumeBox.on('mousedown',function(){
+        _this.selectMenu = function (num, _bool) {
 
-			if(_this.audio){
-				var Y,EndY = parseInt(volumeBox.css('height')),goY;
-				volumeBox.on('mousemove click',function(e){
+            songEq = num;
+            _this.createAudio();
+            (_bool) && _this.playAudio();
+        }
 
-					clearTimeout(volumeTime);
+        _this.volumeSet = function () {
 
-					Y = (e.clientY-(volumeBox.offset().top-$(document).scrollTop()));
-					Y = (Y > 0) ? (Y > EndY) ? EndY : Y : 0;
-	
-					goY = Y/EndY;
-					
-					volumeSize = 1 - goY;
+            _this.audio.volume = volumeSize;
+            volumeSetbacks.css({
+                'height': volumeSize * 100 + "%"
+            });
+        }
 
-					_this.volumeSet();
-				});
-				
-				volumeBox.one('mouseup',function(){
+        _this.newSong = function (_new, _bool) {
 
-					volumeBox.unbind('mousemove');
-				}).on('mouseout',function(){
+            if (typeof (_new) == 'object') {
 
-					volumeTime = setTimeout(function(){
+                if (_new.src) {
 
-						volumeBox.unbind('mousemove');
-					},500);
-				});
-			}
-		});
+                    if (_this.song) {
 
-		setbacks.on('mousedown',function(){
+                        _this.song.push(_new);
+                    } else {
 
-			if(_this.audio){
-				var X,EndX = parseInt(setbacks.css('width')),goX,mouseTime;
-				setbacks.on('mousemove click',function(e){
+                        _this.song = [_new];
+                    }
 
-					_this.stopAudio();
-					clearTimeout(mouseTime);
+                    _this.upMenu();
+                    (_bool) && _this.selectMenu(_this.song.length - 1, true);
+                } else {
 
-					X = (e.clientX-setbacks.offset().left);
-					X = (X > 0) ? (X > EndX) ? EndX : X : 0;
-					
-					goX = X/EndX;
-					thisSetbacks.css({
-						'width' : goX*100+"%"
-					});
-					
-					_this.audio.currentTime = parseInt(goX*_this.audio.duration);
-					thisTime.text(_this.conversion(_this.audio.currentTime));
-				});
-				
-				setbacks.one('mouseup',function(){
+                    alert('提示：对象缺省src属性');
+                }
+            } else {
 
-					_this.playAudio();
-					setbacks.unbind('mousemove');
-				}).on('mouseout',function(){
+                alert('提示：这不是一个对象');
+            }
+        }
 
-					mouseTime = setTimeout(function(){
+        var volumeTime;
+        volumeBox.on('mousedown', function () {
 
-						_this.playAudio();
-						setbacks.unbind('mousemove');
-					},500);
-				});
-			}
-		});
+            if (_this.audio) {
+                var Y, EndY = parseInt(volumeBox.css('height')), goY;
+                volumeBox.on('mousemove click', function (e) {
 
-		play.one('click',function(){
+                    clearTimeout(volumeTime);
 
-			_this.playAudio();
-		});
-		
-		menuBtn.on('click',function(){
+                    Y = (e.clientY - (volumeBox.offset().top - $(document).scrollTop()));
+                    Y = (Y > 0) ? (Y > EndY) ? EndY : Y : 0;
 
-			$(_this.menu.menuView).toggleClass('menu-show');
-		});
+                    goY = Y / EndY;
 
-		prev.on('click',function(){
+                    volumeSize = 1 - goY;
 
-			--songEq;
-			songEq = (songEq >= 0) ? songEq :  _this.song.length -1;
-			_this.selectMenu(songEq,true);
-		});
+                    _this.volumeSet();
+                });
 
-		next.on('click',function(){
+                volumeBox.one('mouseup', function () {
 
-			++songEq;
-			songEq = (songEq < _this.song.length) ? songEq : 0;
-			_this.selectMenu(songEq,true);
-		});
+                    volumeBox.unbind('mousemove');
+                }).on('mouseout', function () {
 
-		menuClose.on('click',function(){
+                    volumeTime = setTimeout(function () {
 
-			$(_this.menu.menuView).removeClass('menu-show');
-		});
+                        volumeBox.unbind('mousemove');
+                    }, 500);
+                });
+            }
+        });
 
-		volume.on('click',function(){
+        setbacks.on('mousedown', function () {
 
-			$(_this.volume.volumeView).toggleClass('audio-show-volume');
-		});
+            if (_this.audio) {
+                var X, EndX = parseInt(setbacks.css('width')), goX, mouseTime;
+                setbacks.on('mousemove click', function (e) {
 
-		_this.upMenu();
+                    _this.stopAudio();
+                    clearTimeout(mouseTime);
 
-		_this.selectMenu(songEq,_this.autoPlay);
-		
-		return _this;
-	}
+                    X = (e.clientX - setbacks.offset().left);
+                    X = (X > 0) ? (X > EndX) ? EndX : X : 0;
+
+                    goX = X / EndX;
+                    thisSetbacks.css({
+                        'width': goX * 100 + "%"
+                    });
+
+                    _this.audio.currentTime = parseInt(goX * _this.audio.duration);
+                    thisTime.text(_this.conversion(_this.audio.currentTime));
+                });
+
+                setbacks.one('mouseup', function () {
+
+                    _this.playAudio();
+                    setbacks.unbind('mousemove');
+                }).on('mouseout', function () {
+
+                    mouseTime = setTimeout(function () {
+
+                        _this.playAudio();
+                        setbacks.unbind('mousemove');
+                    }, 500);
+                });
+            }
+        });
+
+        play.one('click', function () {
+
+            _this.playAudio();
+        });
+
+
+
+
+        menuBtn.on('click', function () {
+
+            $(_this.menu.menuView).toggleClass('menu-show');
+        });
+
+        prev.on('click', function () {
+
+            --songEq;
+            songEq = (songEq >= 0) ? songEq : _this.song.length - 1;
+            _this.selectMenu(songEq, true);
+        });
+
+        next.on('click', function () {
+
+            ++songEq;
+            songEq = (songEq < _this.song.length) ? songEq : 0;
+            _this.selectMenu(songEq, true);
+        });
+
+        menuClose.on('click', function () {
+
+            $(_this.menu.menuView).removeClass('menu-show');
+        });
+
+        volume.on('click', function () {
+
+            $(_this.volume.volumeView).toggleClass('audio-show-volume');
+        });
+
+        _this.upMenu();
+
+        _this.selectMenu(songEq, _this.autoPlay);
+
+        return _this;
+    }
 })(jQuery)
